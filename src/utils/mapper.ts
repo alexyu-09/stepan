d41283e22b2d5d54fdadb21a40e7c350fdad129c
alexyu-09 <yushenko.alexander@gmail.com>
2026-03-02 21:20:20 +0100

    Fix: correctly assign Примітки to indices 39 and 40 after column removal

--- src/utils/mapper.ts
+++ src/utils/mapper.ts
@@ -115,9 +115,9 @@ export const mapToSchema = async (rawRows: RawRow[], skuPrefix: string): Promise
             }
 
             // Index 41: Примітки(RU)
-            newRow[41] = `<p><span style="background-color:#f1c40f;">От 50-100 шт спец цена&nbsp;</span><br /><br /><span style="background-color:#f1c40f;">Достпна оплата по безналу</span></p><p><span style="background-color:#f1c40f;">Для больших заказов - оптовые цены и наличие уточняте у менеджера.</span></p>`;
+            newRow[39] = `<p><span style="background-color:#f1c40f;">От 50-100 шт спец цена&nbsp;</span><br /><br /><span style="background-color:#f1c40f;">Достпна оплата по безналу</span></p><p><span style="background-color:#f1c40f;">Для больших заказов - оптовые цены и наличие уточняте у менеджера.</span></p>`;
             // Index 42: Примітки(UA)
-            newRow[42] = `<p><span style="background-color:#f1c40f;">Від 50-100 шт спец ціна</span></p><p><span style="background-color:#f1c40f;">Достпна оплата по безготівці</span></p><p><span style="background-color:#f1c40f;">Для великих замовлень &ndash; оптові ціни та наявність уточнюйте у менеджера.</span></p>`;
+            newRow[40] = `<p><span style="background-color:#f1c40f;">Від 50-100 шт спец ціна</span></p><p><span style="background-color:#f1c40f;">Достпна оплата по безготівці</span></p><p><span style="background-color:#f1c40f;">Для великих замовлень &ndash; оптові ціни та наявність уточнюйте у менеджера.</span></p>`;
         }
 
         // Try to map price and brand
